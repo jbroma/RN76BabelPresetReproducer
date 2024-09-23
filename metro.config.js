123f616053a351cc60357717894298764e4f2096
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-09-23 14:02:24 +0200

    disable hermesParser

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,10 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    hermesParser: false,
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
